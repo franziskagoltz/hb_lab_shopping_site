3eca4c60e4fede733a8f0da5de178b59eb2b9600
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-12 16:35:02 -0700

    Implemented flash on cart page, fixed melon unique pages

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -92,12 +92,13 @@ def add_to_cart(melon_id):
     # TODO: Finish shopping cart functionality
 
     # The logic here should be something like:
-    #
-    # - check if a "cart" exists in the session, and create one (an empty
-    #   dictionary keyed to the string "cart") if not
-    # - check if the desired melon id is the cart, and if not, put it in
-    # - increment the count for that melon id by 1
-    # - flash a success message
+    print session
+    session["cart"] = session.get("cart", {})
+    session["cart"][melon_id] = session["cart"].get(melon_id, 0) + 1
+    flash("Melon successfully added to cart!!")
+    print session
+    return render_template("cart.html")
+
     # - redirect the user to the cart page
 
     return "Oops! This needs to be implemented!"
